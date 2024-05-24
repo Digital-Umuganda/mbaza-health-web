6b51c83cfa2aec572f39a63ec08881a846a3787a
Celestin Niyindagiriye <nicelestin08@gmail.com>
2024-05-24 17:23:56 +0200

    update project title name

--- src/routes/LinguistRoute.tsx
+++ src/routes/LinguistRoute.tsx
@@ -90,7 +90,7 @@ const LinguistRoute = () => {
                   alt="BAZA Logo"
                 />
                 <span className="sr-only self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                  RBC-Baza
+                  RBC-Mbaza
                 </span>
               </Link>
             </div>
